d3a8b6be03c7dde627e3ee8cb35d65d5603e5d1a
Stanislav Grokhotov <grokhotov@isands.ru>
2023-03-27 19:32:08 +0300

    added part of solution PersonApi

--- src/main/java/io/ylab/intensive/lesson04/eventsourcing/api/PersonApiImpl.java
+++ src/main/java/io/ylab/intensive/lesson04/eventsourcing/api/PersonApiImpl.java
@@ -1,30 +1,71 @@
 package io.ylab.intensive.lesson04.eventsourcing.api;
 
-import java.util.List;
-
 import io.ylab.intensive.lesson04.eventsourcing.Person;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Тут пишем реализацию
  */
+
+
+//TODO проверь что все закрыл!!
 public class PersonApiImpl implements PersonApi {
-  @Override
-  public void deletePerson(Long personId) {
-    
-  }
+    private DataSource dataSource;
 
-  @Override
-  public void savePerson(Long personId, String firstName, String lastName, String middleName) {
+    public PersonApiImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-  }
+    @Override
+    public void deletePerson(Long personId) {
 
-  @Override
-  public Person findPerson(Long personId) {
-    return null;
-  }
+    }
 
-  @Override
-  public List<Person> findAll() {
-    return null;
-  }
+    @Override
+    public void savePerson(Long personId, String firstName, String lastName, String middleName) {
+
+    }
+
+    @Override
+    public Person findPerson(Long personId) throws SQLException {
+        Person person = null;
+        String selectQuery = "SELECT * FROM person WHERE person_id = ?;";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+            preparedStatement.setLong(1, personId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                person = new Person(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+            }
+            resultSet.close();
+            return person;
+        }
+    }
+
+    @Override
+    public List<Person> findAll() throws SQLException {
+        List<Person> people = new LinkedList<>();
+        String selectQuery = "SELECT * FROM person;";
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectQuery)) {
+            while (resultSet.next()) {
+                Person temp = new Person(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4));
+                people.add(temp);
+            }
+        }
+        return people;
+    }
 }
--- src/main/java/io/ylab/intensive/lesson04/movie/MovieLoaderImpl.java
+++ src/main/java/io/ylab/intensive/lesson04/movie/MovieLoaderImpl.java
@@ -1,105 +1,108 @@
 package io.ylab.intensive.lesson04.movie;
 
-import java.io.*;
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
-import javax.sql.DataSource;
 
 public class MovieLoaderImpl implements MovieLoader {
-  private DataSource dataSource;
+    private DataSource dataSource;
 
-  public MovieLoaderImpl(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
-
-  @Override
-  public void loadData(File file) throws IOException{
-    try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-      bufferedReader.readLine();
-      bufferedReader.readLine();
-      String line;
-      while ((line = bufferedReader.readLine()) != null) {
-        String[] parsedLine = line.split(";");
-        Integer year = parsedLine[0].equals("") ? null : Integer.parseInt(parsedLine[0]);
-        Integer length = parsedLine[1].equals("") ? null : Integer.parseInt(parsedLine[1]);
-        String title = parsedLine[2].equals("") ? null : parsedLine[2];
-        String subject = parsedLine[3].equals("") ? null : parsedLine[3];
-        String actors = parsedLine[4].equals("") ? null : parsedLine[4];
-        String actress = parsedLine[5].equals("") ? null : parsedLine[5];
-        String director = parsedLine[6].equals("") ? null : parsedLine[6];
-        Integer popularity  = parsedLine[7].equals("") ? null : Integer.parseInt(parsedLine[7]);
-        Boolean awards = parsedLine[8].equals("") ? null : Boolean.parseBoolean(parsedLine[8]);
-        Movie movie = Movie.builder()
-                .withYear(year)
-                .withLength(length)
-                .withTitle(title)
-                .withSubject(subject)
-                .withActors(actors)
-                .withActress(actress)
-                .withDirector(director)
-                .withPopularity(popularity)
-                .withAwards(awards)
-                .build();
-        saveMovie(movie, dataSource);
-      }
+    public MovieLoaderImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
-  }
 
-  private void saveMovie(Movie movie, DataSource dataSource) {
-    String insertQuery = "INSERT INTO movie (year, length, title, subject, actors, actress, director, popularity, awards)" +
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    try (Connection connection = dataSource.getConnection();
-         PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-      if (movie.getYear() == null) {
-        preparedStatement.setNull(1, Types.INTEGER);
-      } else {
-        preparedStatement.setInt(1, movie.getYear());
-      }
-      if (movie.getLength() == null) {
-        preparedStatement.setNull(2, Types.INTEGER);
-      } else {
-        preparedStatement.setInt(2, movie.getLength());
-      }
-      if (movie.getTitle() == null) {
-        preparedStatement.setNull(3, Types.VARCHAR);
-      } else {
-        preparedStatement.setString(3, movie.getTitle());
-      }
-      if (movie.getSubject() == null) {
-        preparedStatement.setNull(4, Types.VARCHAR);
-      } else {
-        preparedStatement.setString(4, movie.getSubject());
-      }
-      if (movie.getActors() == null) {
-        preparedStatement.setNull(5, Types.VARCHAR);
-      } else {
-        preparedStatement.setString(5, movie.getActors());
-      }
-      if (movie.getActress() == null) {
-        preparedStatement.setNull(6, Types.VARCHAR);
-      } else {
-        preparedStatement.setString(6, movie.getActress());
-      }
-      if (movie.getDirector() == null) {
-        preparedStatement.setNull(7, Types.VARCHAR);
-      } else {
-        preparedStatement.setString(7, movie.getDirector());
-      }
-      if (movie.getPopularity() == null) {
-        preparedStatement.setNull(8, Types.INTEGER);
-      } else {
-        preparedStatement.setInt(8, movie.getPopularity());
-      }
-      if (movie.getAwards() == null) {
-        preparedStatement.setNull(9, Types.BOOLEAN);
-      } else {
-        preparedStatement.setBoolean(9, movie.getAwards());
-      }
-      preparedStatement.executeUpdate();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+    @Override
+    public void loadData(File file) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            bufferedReader.readLine();
+            bufferedReader.readLine();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parsedLine = line.split(";");
+                Integer year = parsedLine[0].equals("") ? null : Integer.parseInt(parsedLine[0]);
+                Integer length = parsedLine[1].equals("") ? null : Integer.parseInt(parsedLine[1]);
+                String title = parsedLine[2].equals("") ? null : parsedLine[2];
+                String subject = parsedLine[3].equals("") ? null : parsedLine[3];
+                String actors = parsedLine[4].equals("") ? null : parsedLine[4];
+                String actress = parsedLine[5].equals("") ? null : parsedLine[5];
+                String director = parsedLine[6].equals("") ? null : parsedLine[6];
+                Integer popularity = parsedLine[7].equals("") ? null : Integer.parseInt(parsedLine[7]);
+                Boolean awards = parsedLine[8].equals("") ? null : Boolean.parseBoolean(parsedLine[8]);
+                Movie movie = Movie.builder()
+                        .withYear(year)
+                        .withLength(length)
+                        .withTitle(title)
+                        .withSubject(subject)
+                        .withActors(actors)
+                        .withActress(actress)
+                        .withDirector(director)
+                        .withPopularity(popularity)
+                        .withAwards(awards)
+                        .build();
+                saveMovie(movie, dataSource);
+            }
+        }
     }
-  }
+
+    private void saveMovie(Movie movie, DataSource dataSource) {
+        String insertQuery = "INSERT INTO movie (year, length, title, subject, actors, actress, director, popularity, awards)" +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            if (movie.getYear() == null) {
+                preparedStatement.setNull(1, Types.INTEGER);
+            } else {
+                preparedStatement.setInt(1, movie.getYear());
+            }
+            if (movie.getLength() == null) {
+                preparedStatement.setNull(2, Types.INTEGER);
+            } else {
+                preparedStatement.setInt(2, movie.getLength());
+            }
+            if (movie.getTitle() == null) {
+                preparedStatement.setNull(3, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(3, movie.getTitle());
+            }
+            if (movie.getSubject() == null) {
+                preparedStatement.setNull(4, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(4, movie.getSubject());
+            }
+            if (movie.getActors() == null) {
+                preparedStatement.setNull(5, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(5, movie.getActors());
+            }
+            if (movie.getActress() == null) {
+                preparedStatement.setNull(6, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(6, movie.getActress());
+            }
+            if (movie.getDirector() == null) {
+                preparedStatement.setNull(7, Types.VARCHAR);
+            } else {
+                preparedStatement.setString(7, movie.getDirector());
+            }
+            if (movie.getPopularity() == null) {
+                preparedStatement.setNull(8, Types.INTEGER);
+            } else {
+                preparedStatement.setInt(8, movie.getPopularity());
+            }
+            if (movie.getAwards() == null) {
+                preparedStatement.setNull(9, Types.BOOLEAN);
+            } else {
+                preparedStatement.setBoolean(9, movie.getAwards());
+            }
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
