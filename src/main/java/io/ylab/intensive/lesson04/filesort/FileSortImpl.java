64c6330897d3b323ebdafac4a7a474511861c5e0
Stanislav Grokhotov <grokhotov@isands.ru>
2023-03-27 16:19:45 +0300

    added solution for fileSorterReturn

--- src/main/java/io/ylab/intensive/lesson04/filesort/FileSortImpl.java
+++ src/main/java/io/ylab/intensive/lesson04/filesort/FileSortImpl.java
@@ -1,18 +1,64 @@
 package io.ylab.intensive.lesson04.filesort;
 
-import java.io.File;
 import javax.sql.DataSource;
+import java.io.*;
+import java.sql.*;
 
 public class FileSortImpl implements FileSorter {
-  private DataSource dataSource;
+    private DataSource dataSource;
 
-  public FileSortImpl(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
+    public FileSortImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-  @Override
-  public File sort(File data) {
-    // ТУТ ПИШЕМ РЕАЛИЗАЦИЮ
-    return null;
-  }
+    @Override
+    public File sort(File data) throws SQLException, IOException {
+        insertValuesIntoTable(data, dataSource);
+        data = refreshFile(data);
+        sortValuesFromTable(data, dataSource);
+        return data;
+    }
+
+    private void insertValuesIntoTable(File data, DataSource dataSource) throws SQLException, IOException {
+        int batchSize = 10000;
+        int size = 0;
+        String insertQuery = "INSERT INTO numbers VALUES (?);";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+             BufferedReader bufferedReader = new BufferedReader(new FileReader(data))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Long value = Long.parseLong(line);
+                preparedStatement.setLong(1, value);
+                preparedStatement.addBatch();
+                if (++size % batchSize == 0) {
+                    preparedStatement.executeBatch();
+                }
+            }
+            preparedStatement.executeBatch();
+        }
+    }
+
+    private File refreshFile(File data) throws IOException {
+        String dataPath = data.getAbsolutePath();
+        data.delete();
+        File newData = new File(dataPath);
+        newData.createNewFile();
+        return newData;
+    }
+
+    private void sortValuesFromTable(File data, DataSource dataSource) throws SQLException, IOException {
+        String selectQuery = "SELECT val FROM numbers ORDER BY val DESC;";
+        String dropQuery = "DROP TABLE numbers;";
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectQuery);
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(data))) {
+            while (resultSet.next()) {
+                Long value = resultSet.getLong("val");
+                bufferedWriter.write(value + "\n");
+            }
+            statement.executeUpdate(dropQuery);
+        }
+    }
 }
--- src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
+++ src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
@@ -1,36 +1,36 @@
 package io.ylab.intensive.lesson04.persistentmap;
 
-import java.sql.SQLException;
-import javax.sql.DataSource;
-
 import io.ylab.intensive.lesson04.DbUtil;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 public class PersistenceMapTest {
-  public static void main(String[] args) throws SQLException {
-    DataSource dataSource = initDb();
-    PersistentMap persistentMap = new PersistentMapImpl(dataSource);
-    persistentMap.init("first map");
-    persistentMap.put("11", "11");
-    persistentMap.put("vdvd", null);
-    persistentMap.put("22", "bfbf");
-    System.out.println(persistentMap.getKeys());
-    System.out.println(persistentMap.containsKey("vdvd"));
-    System.out.println(persistentMap.containsKey("22"));
-    persistentMap.remove("11");
-    System.out.println(persistentMap.getKeys());
-    persistentMap.clear();
-  }
-  
-  public static DataSource initDb() throws SQLException {
-    String createMapTable = "" 
-                                + "drop table if exists persistent_map; " 
-                                + "CREATE TABLE if not exists persistent_map (\n"
-                                + "   map_name varchar,\n"
-                                + "   KEY varchar,\n"
-                                + "   value varchar\n"
-                                + ");";
-    DataSource dataSource = DbUtil.buildDataSource();
-    DbUtil.applyDdl(createMapTable, dataSource);
-    return dataSource;
-  }
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = initDb();
+        PersistentMap persistentMap = new PersistentMapImpl(dataSource);
+        persistentMap.init("first map");
+        persistentMap.put("11", "11");
+        persistentMap.put("vdvd", null);
+        persistentMap.put("22", "bfbf");
+        System.out.println(persistentMap.getKeys());
+        System.out.println(persistentMap.containsKey("vdvd"));
+        System.out.println(persistentMap.containsKey("22"));
+        persistentMap.remove("11");
+        System.out.println(persistentMap.getKeys());
+        persistentMap.clear();
+    }
+
+    public static DataSource initDb() throws SQLException {
+        String createMapTable = ""
+                + "drop table if exists persistent_map; "
+                + "CREATE TABLE if not exists persistent_map (\n"
+                + "   map_name varchar,\n"
+                + "   KEY varchar,\n"
+                + "   value varchar\n"
+                + ");";
+        DataSource dataSource = DbUtil.buildDataSource();
+        DbUtil.applyDdl(createMapTable, dataSource);
+        return dataSource;
+    }
 }
