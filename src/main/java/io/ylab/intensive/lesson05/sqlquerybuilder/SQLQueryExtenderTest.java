2041feecabb60860b0049d1db733fd584a23fcdd
Stanislav Grokhotov <stanislav.skii2000@mail.ru>
2023-04-04 02:02:09 +0700

    added 2 task solution

--- src/main/java/io/ylab/intensive/lesson05/sqlquerybuilder/SQLQueryExtenderTest.java
+++ src/main/java/io/ylab/intensive/lesson05/sqlquerybuilder/SQLQueryExtenderTest.java
@@ -1,20 +1,27 @@
 package io.ylab.intensive.lesson05.sqlquerybuilder;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+@Slf4j
 public class SQLQueryExtenderTest {
-  public static void main(String[] args) throws SQLException {
-    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
-    applicationContext.start();
-    SQLQueryBuilder queryBuilder = applicationContext.getBean(SQLQueryBuilder.class);
-    List<String> tables = queryBuilder.getTables();
-    // вот так сгенерируем запросы для всех таблиц что есть в БД
-    for (String tableName : tables) {
-      System.out.println(queryBuilder.queryForTable(tableName));
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+        applicationContext.start();
+        try {
+            SQLQueryBuilder queryBuilder = applicationContext.getBean(SQLQueryBuilder.class);
+            List<String> tables = queryBuilder.getTables();
+            // вот так сгенерируем запросы для всех таблиц что есть в БД
+            for (String tableName : tables) {
+                System.out.println(queryBuilder.queryForTable(tableName));
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        } finally {
+            applicationContext.close();
+        }
     }
-  }
 }
