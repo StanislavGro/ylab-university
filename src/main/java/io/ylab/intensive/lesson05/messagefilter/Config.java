ae4b90f9cd496e1fa47bed427ff8ee847d61c1b0
Stanislav Grokhotov <grokhotov@isands.ru>
2023-04-04 21:13:44 +0300

    fixed bugs of 3 task

--- src/main/java/io/ylab/intensive/lesson05/messagefilter/Config.java
+++ src/main/java/io/ylab/intensive/lesson05/messagefilter/Config.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PreDestroy;
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("io.ylab.intensive.lesson05.messagefilter")
 public class Config {
+    private DataSource dataSource;
+
     @Bean
     public ConnectionFactory connectionFactory() {
         ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -30,6 +33,14 @@ public class Config {
         dataSource.setPassword("postgres");
         dataSource.setDatabaseName("postgres");
         dataSource.setPortNumber(5432);
+        this.dataSource = dataSource;
         return dataSource;
+    }
+
+    @PreDestroy
+    public void destroy() throws Exception {
+        if (this.dataSource != null) {
+            this.dataSource.getConnection().close();
+        }
     }
 }
